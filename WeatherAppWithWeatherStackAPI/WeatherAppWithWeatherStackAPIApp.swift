01a50549f934e803fda31a2f763c137c12913002
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2023-05-18 21:21:22 +0300

    Initial Commit

---
+++ WeatherAppWithWeatherStackAPI/WeatherAppWithWeatherStackAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppWithWeatherStackAPIApp.swift
+//  WeatherAppWithWeatherStackAPI
+//
+//  Created by Камаль Атавалиев on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppWithWeatherStackAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
